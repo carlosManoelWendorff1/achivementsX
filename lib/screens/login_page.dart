ff570975cd9d0fb82b76199d48f29ec06cd554d1
CarlosManoelWendorff <carlos.wendorff@catolicasc.edu.br>
2023-11-01 21:12:54 -0300

    google and facebook basic auth

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,32 +1,23 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:x_axhievments/firebase_auth.dart';
 import 'package:x_axhievments/screens/register_page.dart';
-
 import '../widgets/form_container.dart';
 
-
-
-
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-  bool _isSigning = false;
-
-
-
   final FirebaseAuthService _auth = FirebaseAuthService();
-
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-
 
   @override
   void dispose() {
@@ -35,6 +26,11 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,13 +53,13 @@ class _LoginPageState extends State<LoginPage> {
                 hintText: "Email",
                 isPasswordField: false,
               ),
-              SizedBox(height: 10,),
+              SizedBox(height: 10),
               FormContainerWidget(
                 controller: _passwordController,
                 hintText: "Senha",
                 isPasswordField: true,
               ),
-              SizedBox(height: 30,),
+              SizedBox(height: 30),
               GestureDetector(
                 onTap: _signIn,
                 child: Container(
@@ -73,23 +69,77 @@ class _LoginPageState extends State<LoginPage> {
                     color: Colors.green,
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Center(child:Text("Login",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 20,),
-              Row(mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text("Não tem uma conta?"),
-                  SizedBox(width: 5,),
+                  SizedBox(width: 5),
                   GestureDetector(
-                      onTap: (){
-                        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => SignUpPage()), (route) => false);
-                      },
-                      child: Text("Criar conta",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold),))
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                        (route) => false,
+                      );
+                    },
+                    child: Text(
+                      "Criar conta",
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
                 ],
-              )
-
-
+              ),
+              SizedBox(height: 20),
+              // Google Sign-In Button
+              ElevatedButton(
+                onPressed: _signInWithGoogle,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // Change color as needed
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.g_translate),
+                    SizedBox(width: 8),
+                    Text("Sign in with Google"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              // Facebook Sign-In Button
+              ElevatedButton(
+                onPressed: _signInWithFacebook,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // Change color as needed
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.facebook),
+                    SizedBox(width: 8),
+                    Text("Sign in with Facebook"),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -98,25 +148,66 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void _signIn() async {
-
     String email = _emailController.text;
     String password = _passwordController.text;
-
     User? user = await _auth.signInWithEmailAndPassword(email, password);
-    print(user);
-
-
-    if (user!= null){
-      print("User is successfully signedIn");
+    if (user != null) {
       Navigator.pushNamed(context, "/gameList");
-    } else{
-      print("Some error happend");
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text("Email ou senha incorretos"),
+          content: Text("Email or password is incorrect"),
         ),
       );
     }
+  }
 
+  void _signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) {
+      return;
+    }
+
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final UserCredential authResult =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    final User? user = authResult.user;
+
+    if (user != null) {
+      Navigator.pushNamed(context, "/gameList");
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Google Sign-In failed"),
+        ),
+      );
+    }
+  }
+
+  void _signInWithFacebook() async {
+    Navigator.pushNamed(context, "/gameList");
+    final result = await FacebookAuth.instance.login();
+    if (result.status == LoginStatus.success) {
+      final AuthCredential credential =
+          FacebookAuthProvider.credential(result.accessToken!.token);
+      final UserCredential authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      final User? user = authResult.user;
+
+      if (user != null) {
+        Navigator.pushNamed(context, "/gameList");
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Facebook Sign-In failed"),
+          ),
+        );
+      }
+    }
   }
 }
